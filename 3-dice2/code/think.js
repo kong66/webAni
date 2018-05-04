c7bef1500e8c808a6af9c7e4e8361d08803c5d6a
kong66 <konglingbin66@outlook.com>
2018-05-04 17:22:20 +0800

    dice2

--- 3-dice2/code/think.js
+++ 3-dice2/code/think.js
@@ -1,31 +1,36 @@
 ﻿jQuery(function($){
-	var $btns = $('.btn-group'),
+	var $btnGroup = $('.btn-group'),
 			$cube = $('.cube'),
 			$dice = $('.dice');
-	$btns.on('click',onclick_btn);
+	$btnGroup.on('click',onclick_btn);
 	$cube.on('transitionend',onAniEnd);
 
 	function onclick_btn(e){
 		var point,
-				prePoint,
-				pointStr,
-				num,
 				$btn;
 		if(!$dice.hasClass('ani') && e.target.tagName =="BUTTON"){
 			$btn = $(e.target);
 			point = parseInt($btn.attr('point'));
-			pointStr = $cube.attr('point').match(/\d/);
-			num = pointStr && pointStr.length>0 && pointStr[0];
-			prePoint = parseInt(num);
-			if(prePoint != point){
-				$cube.attr("point","p"+point);
+			if(setPoint(point)){
 				$dice.addClass('ani');
-				// console.log('ani');
 			}
 		}
 	}
+	function setPoint(point){
+		var prePoint,
+				pointStr,
+				num;
+		pointStr = $cube.attr('point').match(/\d/);
+		num = pointStr && pointStr.length>0 && pointStr[0];
+		prePoint = parseInt(num);
+		if(prePoint != point){
+			$cube.attr("point","p"+point);
+			return true;
+		}else {
+			return false;
+		}
+	}
 	function onAniEnd(e){
-		// console.log("xxx");
 		$dice.removeClass('ani');
 	}
 
