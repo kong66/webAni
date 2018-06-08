86dfdef32766c281aeb8c841c1f2f5b5742ad45b
kong66 <konglingbin66@outlook.com>
2018-06-08 19:05:31 +0800

    hoolowed

---
+++ 8-hollowed-out-text/code/think.js
@@ -0,0 +1,34 @@
+jQuery(
+  function($){
+    var $tex = $('.text'),
+        $win = $(window),
+        $doc = $(document),
+        cw,
+        ch,
+        imgW,
+        imgH;
+
+    init();
+    $win.on("resize",onResize);
+    $doc.on("mousemove",onMouseMove);
+
+    function onResize(){
+      init();
+    }
+    function init(){
+      cw = $win.width();
+      ch = $win.height();
+      imgW = $tex.css("background-size");
+    }
+    function onMouseMove(e){
+      var x = e.clientX/cw*100,
+          y = e.clientY/ch*100;
+      console.log("x,y="+x+","+y);
+      $tex.css(
+        {
+          backgroundPosition:x+"% "+ y+"%",
+        }
+      );
+    }
+  }
+);
