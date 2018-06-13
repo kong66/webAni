a72eff2e61495cd2d8d1230964910de4fa913500
kong66 <konglingbin66@outlook.com>
2018-06-13 16:13:28 +0800

    svg

---
+++ 8-hollowed-out-text/code2/think.js
@@ -0,0 +1,33 @@
+jQuery(
+  // function($){
+  //   var $tex = $('.text-frame'),
+  //       $win = $(window),
+  //       $doc = $(document),
+  //       cw,
+  //       ch,
+  //       imgW,
+  //       imgH;
+  //
+  //   init();
+  //   $win.on("resize",onResize);
+  //   $doc.on("mousemove",onMouseMove);
+  //
+  //   function onResize(){
+  //     init();
+  //   }
+  //   function init(){
+  //     cw = $win.width();
+  //     ch = $win.height();
+  //     imgW = $tex.css("background-size");
+  //   }
+  //   function onMouseMove(e){
+  //     var x = e.clientX/cw*100,
+  //         y = e.clientY/ch*100;
+  //     $tex.css(
+  //       {
+  //         backgroundPosition:x+"% "+ y+"%",
+  //       }
+  //     );
+  //   }
+  // }
+);
