5dd00dc0615eb7740ff1c3ea6136883ffebfceea
kong66 <konglingbin66@outlook.com>
2018-08-08 11:52:24 +0800

    18

--- 18-svg-filter-water-wave/code/think.js
+++ 18-svg-filter-water-wave/code/think.js
@@ -12,20 +12,36 @@ jQuery(function($){
 
   $svg.on("click",function(e){
     var i,
-        x = (e.pageX - svgL)/svgW*1000,
-        y = (e.pageY - svgT)/svgH*1000;
-        console.log("x,y:"+x+","+y);
+        $ani,
+        $newRipple,
+        matrix = $svg[0].getScreenCTM().inverse(),
+        svgPoint = $svg[0].createSVGPoint(),
+        svgXY;
+        svgPoint.x = e.clientX;
+        svgPoint.y = e.clientY;
+        svgXY = svgPoint.matrixTransform(matrix);
+
+        console.log("x,y:"+e.clientX+","+e.clientY);
+        console.log("px,py:"+svgXY.x+","+svgXY.y);
+
         $newRipple = $ripple.clone();
         $ripples.append($newRipple);
-        $newRipple.attr("transform","translate("+x+","+y+")");
+        $newRipple.attr("transform","translate("+svgXY.x+","+svgXY.y+")");
         $ani = $newRipple.find('animate');
+        $newRipple.myEndAniNum = 0;
+        $newRipple.myOnEnd = function(){
+          if(++$newRipple.myEndAniNum==$ani.length){
+            $newRipple.remove();
+            console.log("ani end");
+          }
+          else{
+            console.log("over****");
+          }
+        };
         for(i=0;i<$ani.length;++i){
           $ani[i].beginElement();
+          $ani[i].onend= $newRipple.myOnEnd;
         }
-        // $ani[0].onend = function(){
-        //   $newRipple.remove();
-        // }
-
   });
 
 });
