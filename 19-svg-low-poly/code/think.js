39e9a2856b65e3af9d0b6a64fa187555298e91c4
kong66 <konglingbin66@outlook.com>
2018-08-10 17:49:58 +0800

    19

---
+++ 19-svg-low-poly/code/think.js
@@ -0,0 +1,12 @@
+jQuery(function($){
+
+  var $svg = $('svg'),
+      $triangles = $('svg g polygon');
+
+      $triangles.each(function(){
+        var $this = $(this);
+        
+
+      });
+
+});
