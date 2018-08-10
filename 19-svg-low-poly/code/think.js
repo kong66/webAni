260d7484dea5fc0e30476ea76136335bb8da0b12
kong66 <konglingbin66@outlook.com>
2018-08-10 18:24:46 +0800

    19

--- 19-svg-low-poly/code/think.js
+++ 19-svg-low-poly/code/think.js
@@ -4,9 +4,23 @@ jQuery(function($){
       $triangles = $('svg g polygon');
 
       $triangles.each(function(){
-        var $this = $(this);
-        
+        var $this = $(this),
+            x = Math.random()*300,
+            y = Math.random()*300,
+            svgNS="http://www.w3.org/2000/svg",
+        newAni =document.createElementNS(svgNS,"animateTransform");
+        newAni.setAttribute("attributeName","transform");
+        newAni.setAttribute("type","translate");
+        newAni.setAttribute("from","0,0");
+        newAni.setAttribute("to",x+","+y);
+        newAni.setAttribute("repeatCount","1");
+        newAni.setAttribute("dur","1s");
+        this.appendChild(newAni);
+        // $newAni.attr("begin","indefinite");
 
+        //  $this.append($newAni);
+        // $newAni[0].beginElement();
+        // newAni.beginElement();
       });
 
 });
