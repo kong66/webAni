ca9f79dc3e45358c0acf2cb8dbf1e2c2bfd66e4e
kong66 <konglingbin66@outlook.com>
2018-08-17 09:48:19 +0800

    19

--- 19-svg-low-poly/code/think.js
+++ 19-svg-low-poly/code/think.js
@@ -1,52 +1,113 @@
 jQuery(function($){
 
   var $svg = $('svg'),
-      $triangles = $('svg g polygon');
-
-      $triangles.each(function(){
-        var $this = $(this),
-            x = Math.random()*1000-500,
-            y = Math.random()*1000-500,
-            svgNS="http://www.w3.org/2000/svg",
-            points = this.getAttribute("points"),
-        newAni =document.createElementNS(svgNS,"animateTransform");
-        newAni.setAttribute("attributeName","transform");
-        newAni.setAttribute("type","translate");
-        newAni.setAttribute("from","0,0");
-        newAni.setAttribute("to",x+","+y);
-        newAni.setAttribute("repeatCount","1");
-        newAni.setAttribute("dur","1s");
-        newAni.setAttribute("fill","freeze");
-        newAni.setAttribute("begin","triangles.click");
-        this.appendChild(newAni);
+      $triangles = $('polygon'),
+      svgNS="http://www.w3.org/2000/svg",
+      hueFilters = [document.getElementById("HueRoateA"),
+          document.getElementById("HueRoateB")],
+      curFilter = hueFilters[0],
+      prevFilter = hueFilters[1];
 
 
-        // newAni =document.createElementNS(svgNS,"animateTransform");
-        // var xyArray =
-        // this.getAttribute("points").split(/,|\s/);
-        // var x2 = (xyArray[0]*1+xyArray[2]*1+xyArray[4]*1)/3;
-        // var y2 = (xyArray[1]*1+xyArray[3]*1+xyArray[5]*1)/3;
-        // console.log(xyArray);
-        // newAni.setAttribute("attributeName","transform");
-        // newAni.setAttribute("type","rotate");
-        // newAni.setAttribute("from","0,"+x2+","+y2);
-        // newAni.setAttribute("to","720,"+x+","+y);
-        // newAni.setAttribute("repeatCount","1");
-        // newAni.setAttribute("dur","1s");
-        // newAni.setAttribute("fill","freeze");
-        // newAni.setAttribute("begin","triangles.click");
-        // this.appendChild(newAni);
+  (function(){
+    changeFilter();
+    $triangles.each(function(index){
+      init(this,index);
+    });
+  })();
 
-        newAni =document.createElementNS(svgNS,"animate");
-        newAni.setAttribute("attributeName","fill-opacity");
-        newAni.setAttribute("from","1");
-        newAni.setAttribute("to","0");
-        newAni.setAttribute("repeatCount","1");
-        newAni.setAttribute("dur","1s");
-        newAni.setAttribute("fill","freeze");
-        newAni.setAttribute("begin","triangles.click");
-        this.appendChild(newAni);
+  $svg.on("click",onclickTriangles);
 
-      });
+  function onclickTriangles(){
+    changeFilter();
+    $triangles.each(function(){
+      changePolygonFilter(this);
+    });
+  }
+  function changeFilter(){
+    var hue = Math.round(Math.random()*360);
+    var temp = curFilter;
+    curFilter = prevFilter;
+    prevFilter = temp;
+    $(curFilter).children("feColorMatrix").attr("values", hue+"");
+  }
+  function changePolygonFilter(polygon){
+    polygon.setAttribute("filter",getPrevFilter());
+    var ani = $(polygon).children('set');
+    ani[0].setAttribute("from",getPrevFilter());
+    ani[0].setAttribute("to",getCurFilter());
+  }
+  function getCurFilter(){
+    return "url(#"+curFilter.id+")";
+  }
+  function getPrevFilter(){
+    return "url(#"+prevFilter.id+")";
+  }
 
+  function init(polygon,index){
+    var points;
+    polygon.mycolor = $(polygon).css("fill");
+    points = $(polygon).attr("points").split(/,|\s/),
+    polygon.cx = ((points[0]*1 + points[2]*1 +points[4]*1)/3).toFixed(0),
+    polygon.cy = ((points[1]*1 + points[3]*1 +points[5]*1)/3).toFixed(0);
+    polygon.setAttribute("filter",getCurFilter());
+    createAni(polygon,index);
+  }
+
+
+  function createAni(polygon,index){
+    var begin,dur;
+    dur = (Math.random()*3+0.5).toFixed(1);
+    begin = "triangles.click";
+    createAnimateNode(polygon,index,"fill-opacity",
+      [0,0.5,1],[1,0,1],
+      dur,begin,"freeze","replace");
+
+    createAnimateNode(polygon,index,  "translate",
+      [0,0.5,1],["0,0", polygon.cx+","+polygon.cy,"0,0"],
+      dur,begin,"freeze","sum");
+    createAnimateNode(polygon,index,"scale",
+      [0,0.5,1],[1,0,1],dur,begin,"freeze","sum");
+
+    begin += "+" +(dur/2).toFixed(1) +"s";
+    createAnimateNode(polygon,index,"filter",
+      null,[getPrevFilter(),getCurFilter()],
+      0.1,begin,"freeze","sum");
+  }
+
+  function createAnimateNode(node,index,attr,
+    keyTimes,values,dur,begin,fill,additive){
+    var ani,str;
+    if(attr=="scale" ||
+       attr=="translate" ||
+       attr=="rotate"){
+      ani = document.createElementNS(
+        svgNS,"animateTransform");
+      ani.setAttribute("attributeName","transform");
+      ani.setAttribute("type",attr);
+    }else if(attr=="filter"){
+      ani =document.createElementNS(svgNS,"set");
+      ani.setAttribute("attributeName",attr);
+    }else{
+      ani =document.createElementNS(svgNS,"animate");
+      ani.setAttribute("attributeName",attr);
+    }
+    str = attr.replace("-","");
+    ani.setAttribute("attributeType","XML");
+    ani.setAttribute("id",str+index);
+    if(attr=="filter"){
+      ani.setAttribute("from",values[0]);
+      ani.setAttribute("to",values[1]);
+    }else{
+      ani.setAttribute("keyTimes",keyTimes.join(";"));
+      ani.setAttribute("values",values.join(";"));
+    }
+    ani.setAttribute("repeatCount","1");
+    ani.setAttribute("fill",fill);
+    ani.setAttribute("additive",additive);
+    ani.setAttribute("begin",begin);
+    ani.setAttribute("dur",dur+"s");
+    node.appendChild(ani);
+    return ani;
+  }
 });
