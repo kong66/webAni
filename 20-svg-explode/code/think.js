19759c645ede665e49c0a78c720c2dd0ecb33b4f
kong66 <konglingbin66@outlook.com>
2018-08-21 23:14:02 +0800

    20

--- 20-svg-explode/code/think.js
+++ 20-svg-explode/code/think.js
@@ -1,53 +1,109 @@
 jQuery(function($){
 
   var $svg = $('svg'),
-      $pieces = $('.text path'),
+      $text = $('.text');
+      $letters = $('.text>.letter'),
       $rect = $('rect'),
-      delta = (1000/30).toFixed(2)*1,
-      array = [];
+      frameTime = (1000/30).toFixed(2)*1,
+      array = [],
+      aniNum = 0,
+      timer = null,
+      aniState =0;
 
-  function AniInfo(t,tx,ty,sx,sy,r){
-    this.tx = tx[0];
-    this.ty = tx[0];
-    this.sx = sx[0];
-    $ele.sy = sy[0];
-    $ele.r = r[0];
-    $ele.aniDur = t;
-    $ele.tx_del = ((tx[1] - tx[0])/t*delta);
-    $ele.ty_del = ((ty[1] - ty[0])/t*delta);
-    $ele.sx_del = ((sx[1] - sx[0])/t*delta);
-    $ele.sy_del = ((sy[1] - sy[0])/t*delta);
-    $ele.r_del = ((r[1] - r[0])/t*delta);
+  init();
+
+  function init(){
+    $letters.each(function(n){
+      var $pieces = $(this).find("path");
+      array.push([]);
+      $pieces.each(function(index){
+          var $this = $(this),
+              tx = Math.random()*1000-500,
+              ty = Math.random()*300-150,
+              r = Math.random()*360,
+              dur = Math.random()*1000+2000;
+          array[n].push($this);
+          setAniInfo($this,800*n,dur,
+            [
+              ["tx",[0,tx]  ],
+              ["ty",[0,ty]  ],
+              ["sx",[1,0.5] ],
+              ["sy",[1,0.5] ],
+              ["r", [0,r]   ],
+              ["o", [1,0]   ]
+            ]);
+      });
+    });
+
+    $text.on("click",onClickText);
   }
-  $pieces.each(function(index){
-    //if(index==0)
-    {
-      var $this = $(this),
-          tx = Math.random()*1200-600,
-          ty = Math.random()*1200-600,
-          deg = Math.random()*360,
-          dur = Math.random()*1000+1000;
-      array.push($this);
-      setAniInfo($this,dur,[0,tx],[0,ty],[1,1],[1,1],[0,deg]);
-      // setAniInfo($this,3000,[0,50],[0,50],[1,2],[1,2],[0,135]);
+  function onClickText(){
+    if(aniState==0){
+      onAniState();
     }
-  });
-  window.setInterval(function(){
-    for(var it in array){
-      actAni(array[it]);
+  }
+  function startAni(direction){
+    var letter,it,it2;
+    for(it in array){
+      letter = array[it];
+      for(it2 in letter){
+          letter[it2].ani = true;
+          letter[it2].dir = direction;
+          letter[it2].timer = 0;
+          ++aniNum;
+      }
     }
-  },delta);
+    doAni();
+  }
 
-  // setAniInfo($rect,3000,[0,50],[0,50],[1,2],[1,2],[0,135]);
-  // window.setInterval(function(){
-  //   actAni($rect)
-  // },delta);
+  function onAniState(){
+    if(aniState==0){
+      aniState++;
+      startAni(true);
+    }else if(aniState==1){
+      aniState++;
+      startAni(false);
+    }else{
+      aniState = 0;
+    }
+  }
 
-  function setAniInfo($ele,t,tx,ty,sx,sy,r){
+  function doAni(){
+    timer = window.setInterval(function(){
+      var letter,it,it2;
+      for(it in array){
+        letter = array[it];
+        for(it2 in letter){
+          if(actAni(letter[it2])){
+            --aniNum;
+          }
+        }
+      }
+      if(0 == aniNum){
+        window.clearInterval(timer);
+        timer = 0;
+        onAniState();
+      }
+    },frameTime);
+  }
 
-    var d = $ele[0].getAttribute("d");
-    var str = d.match(/\d+\.?\d*,\d+\.?\d*/);
-    var c = str[0].split(",");
+  function setAniInfo($ele,delay,dur,aniArray){
+    var i;
+    var c = getCenter($ele);
+    $ele.center = c;
+    $ele.dur = dur;
+    $ele.timer = 0;
+    $ele.delay = delay;
+    $ele.ani = false;
+    $ele.dir = true;
+    for(i in aniArray){
+      $ele[aniArray[i][0]] = aniArray[i][1];
+    }
+  }
+  function getCenter($ele){
+    // var d = $ele[0].getAttribute("d");
+    // var str = d.match(/\d+\.?\d*,\d+\.?\d*/);
+    // var c = str[0].split(",");
     //console.log(c);
     // var x = $ele[0].getAttribute("x")*1,
     //     y = $ele[0].getAttribute("y")*1,
@@ -56,62 +112,72 @@ jQuery(function($){
     // //console.log("******"+x+","+y+",");
     // $ele.cx = x*1+width/2;
     // $ele.cy = y*1+height/2;
-    $ele.cx = c[0]*1;
-    $ele.cy = c[1]*1;
-    console.log("cx,cy="+$ele.cx+","+$ele.cy);
-    $ele.tx = tx[0];
-    $ele.ty = tx[0];
-    $ele.sx = sx[0];
-    $ele.sy = sy[0];
-    $ele.r = r[0];
-    $ele.aniDur = t;
-    $ele.tx_del = ((tx[1] - tx[0])/t*delta);
-    $ele.ty_del = ((ty[1] - ty[0])/t*delta);
-    $ele.sx_del = ((sx[1] - sx[0])/t*delta);
-    $ele.sy_del = ((sy[1] - sy[0])/t*delta);
-    $ele.r_del = ((r[1] - r[0])/t*delta);
+    return [0,0];
   }
   function actAni($ele){
-    if($ele.aniDur<=0)
-      return;
-    $ele.aniDur -= delta;
-    $ele.tx += $ele.tx_del;
-    $ele.ty += $ele.ty_del;
-    $ele.sx += $ele.sx_del;
-    $ele.sy += $ele.sy_del;
-    $ele.r  += $ele.r_del;
-    setStyle($ele);
+    var ret = false;
+    if($ele.ani){
+      if($ele.timer <= $ele.dur + $ele.delay ){
+        $ele.timer += frameTime;
+        if($ele.timer>=$ele.delay){
+          setStyle($ele);
+        }
+      }else{
+        $ele.ani = false;
+        ret = true;
+      }
+    }
+    return ret;
   }
 
   function setStyle($ele){
 
-    style = getTranslateStyle($ele.tx,$ele.ty)
-          + getScaleStyle($ele.cx,$ele.cy,$ele.sx,$ele.sy);
+    var transformStyle = getTranslateStyle($ele)
+          + getScaleStyle($ele);
           // + getRotateStyle($ele.cx,$ele.cy,$ele.r);
-    //console.log(style);
-    $ele.css("transform",style);
+
+    $ele.css("transform",transformStyle);
+    $ele.css("stroke-opacity",getOpacityStyle($ele));
+    $ele.css("fill-opacity",getOpacityStyle($ele));
   }
-  function getTranslateStyle(tx,ty){
-    // tx = tx.toFixed(3);
-    // ty = ty.toFixed(3);
+  function getOpacityStyle($ele){
+    var o = getInterpolation($ele,"o");
+    return o+"";
+  }
+  function getTranslateStyle($ele){
+    var tx = getInterpolation($ele,"tx"),
+        ty = getInterpolation($ele,"ty");
     return "translate("+tx+"px,"+ty+"px)";
   }
-  function getScaleStyle(cx,cy,sx,sy){
-    var tx = -cx*(sx - 1),
+  function getScaleStyle($ele){
+    var sx = getInterpolation($ele,"sx"),
+        sy = getInterpolation($ele,"sy"),
+        cx = $ele.center[0],
+        cy = $ele.center[1],
+        tx = -cx*(sx - 1),
         ty = -cy*(sy - 1);
-
-    // tx = tx.toFixed(3);
-    // ty = ty.toFixed(3);
-    // sx = sx.toFixed(3);
-    // sy = sy.toFixed(3);
     return " translate("+tx+"px,"+ty+"px) scale("+sx+","+sy+")";
   }
-  function getRotateStyle(cx,cy,deg){
-    // cx = cx.toFixed(3);
-    // cy = cy.toFixed(3);
-    // deg = deg.toFixed(3);
+  function getRotateStyle($ele){
+    var r = getInterpolation($ele,"r"),
+        cx = $ele.center[0],
+        cy = $ele.center[1];
     return " translate("+cx+"px,"+cy
-      +"px) rotate("+deg+"deg) "
+      +"px) rotate("+r+"deg) "
       +" translate("+(-cx)+"px,"+(-cy)+"px)";
+  }
+  function getInterpolation($ele,attr){
+    var begin = $ele[attr][0],
+        end = $ele[attr][1],
+        t = $ele.timer - $ele.delay,
+        k = t/$ele.dur,
+        res;
+    if(Math.abs(t-$ele.dur)<frameTime){
+      res = $ele.dir ? end:begin;
+    }else{
+      res = $ele.dir ?
+      (begin + (end-begin)*k) : (end + (begin-end)*k);
+    }
+    return res;
   }
 });
