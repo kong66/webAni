a10b010cff1d7258d9ec160e0899106fa2dfd6a4
kong66 <konglingbin66@outlook.com>
2018-08-27 18:41:24 +0800

    21

--- 21-svg-transform/code/think.js
+++ 21-svg-transform/code/think.js
@@ -1,4 +1,197 @@
 jQuery(function($){
 
-  
+  var $path1 = $("#text_a"),
+      $path2 = $("#text_k"),
+      pathes1,
+      pathes2,
+      $ele = $("#abc");
+  pathes1 = pathToValue($path1);
+  toUpperCaseCommand(pathes1);
+  pathes2 = pathToValue($path2);
+  toUpperCaseCommand(pathes2);
+  toSameNumPath(pathes1,pathes2);
+
+  $ele.attr("d",valueToPath(pathes1));
+  var p = $ele.find("animate")[0];
+  p.setAttribute("from",valueToPath(pathes1));
+  p.setAttribute("to",valueToPath(pathes2));
+  p.beginElement();
+
+  function pathToValue($path){
+    var i,
+        j,
+        k,
+        pathes = $path.attr("d").match(/M[^M]*[zZ]/g);
+    for(i=0;i<pathes.length;++i){
+      pathes[i] = pathes[i].match(/[a-zA-Z][^a-zA-Z]*/g);
+      for(j=0;j<pathes[i].length;++j){
+        pathes[i][j]=pathes[i][j].match(/[a-zA-Z]|(-?([^a-zA-Z,\s-])+)/g);
+        for(k=1;k<pathes[i][j].length;++k){
+          pathes[i][j][k] *=1;
+        }
+      }
+    }
+    console.log(pathes);
+    return pathes;
+  }
+  function toUpperCaseCommand(pathes){
+    var i,j,k,points=[];
+    for(i=0;i<pathes.length;++i){
+      for(j=0;j<pathes[i].length;++j){
+        toAbasolutePoint(pathes[i][j],points);
+      }
+    }
+    console.log(pathes);
+  }
+
+  function toAbasolutePoint(command,points){
+    var point=null,
+        pre=[0,0];
+    if(points && points.length>0){
+      pre = points[points.length-1];
+    }
+    switch (command[0]) {
+      case 'M':
+        point=[command[1],command[2]];
+        break;
+      case 'h':
+      case 'H':
+        hToL(command,pre);
+        point=lToC(command,pre);
+        break;
+      case 'v':
+      case 'V':
+        vToL(command,pre);
+        point=lToC(command,pre);
+        break;
+      case 'l':
+      case "L":
+        point=lToC(command,pre);
+        break;
+      case 'c':
+      case 'C':
+        point=cToC(command,pre);
+        break;
+      default:
+        break;
+    }
+    points.push(point);
+
+  }
+  function hToL(command,pre){
+    if(command[0] == 'h'){
+      command[1]+=pre[0];
+    }
+    command[0] = 'L';
+    command[2] = pre[1];
+    return [command[1],command[2]];
+  }
+  function vToL(command,pre){
+    if(command[0] == 'v'){
+      command[1]+=pre[1];
+    }
+    command[0] = 'L';
+    command[2] = command[1];
+    command[1] = pre[0];
+    return [command[1],command[2]];
+  }
+  function lToC(command,pre){
+    if(command[0] == 'l'){
+      command[1]+=pre[0];
+      command[2]+=pre[1];
+    }
+    command[0]='C';
+    command[5] = command[1];
+    command[6] = command[2];
+    command[1] = pre[0];
+    command[2] = pre[1];
+    command[3] = command[5];
+    command[4] = command[6];
+    return [command[5],command[6]];
+  }
+  function cToC(command,pre){
+    if(command[0] == 'c'){
+      command[0] = 'C';
+      command[1] += pre[0];
+      command[2] += pre[1];
+      command[3] += pre[0];
+      command[4] += pre[1];
+      command[5] += pre[0];
+      command[6] += pre[1];
+    }
+    return [command[5],command[6]];
+  }
+  function toSameNumPath(target,source){
+    var i,j,addNum,pathesA,pathesB;
+    if(target.length < source.length){
+      pathesA = target;
+      pathesB = source;
+    }else{
+      pathesB = target;
+      pathesA = source;
+    }
+    addNum = Math.abs(target.length - source.length);
+    for(i=0;i<addNum;++i){
+      pathesA.push(clonePath(pathesA[0]));
+    }
+    for(i=0;i<pathesA.length;++i){
+      for(j=0;j<pathesA[i].length;++j){
+        toSameNumPoints(pathesA[i],pathesB[i]);
+      }
+    }
+  }
+  function clonePath(path){
+    var i,j,newPath =[];
+    for(i=0;i<path.length;++i){
+      newPath.push([]);
+      for(j=0;j<path[i].length;++j){
+        newPath[i].push(path[i][j]);
+      }
+    }
+    return newPath;
+  }
+  function toSameNumPoints(pathA,pathB){
+    var less,i,j,point;
+    if(pathA.length!=pathB.length){
+      if(pathA.length < pathB.length){
+        less = pathA;
+      }else{
+        less = pathB;
+      }
+      addNum = Math.abs(pathA.length - pathB.length);
+      if(less.length>2){
+        point=less[less.length-2];
+        for(i=0;i<addNum;++i){
+          less.splice(less.length-1,0,clonePoint(point));
+        }
+      }
+
+    }
+  }
+  function clonePoint(points){
+    var i,newPoints=[];
+    for(i=0;i<points.length;++i){
+      newPoints.push(points[i]);
+    }
+    return newPoints;
+  }
+
+  function valueToPath(pathes){
+    var i,j,k,str="";
+    for(i=0;i<pathes.length;++i){
+      for(j=0;j<pathes[i].length;++j){
+        for(k=0;k<pathes[i][j].length;++k){
+          str+=pathes[i][j][k];
+          if(k==pathes[i][j].length-1){
+            str+=" ";
+          }else if(k!=0){
+            str+=",";
+          }
+        }
+      }
+    }
+    console.log(str);
+    return str;
+  }
+
 });
