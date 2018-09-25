c1f4f9f6abef7910572373a528eded32dfba118f
kong66 <konglingbin66@outlook.com>
2018-09-25 19:46:05 +0800

    24

--- 24-svg-flowers-erupt/code/think.js
+++ 24-svg-flowers-erupt/code/think.js
@@ -1,23 +1,18 @@
 jQuery(function($){
 
-  var svgNS=$('svg').attr('xmlns'),
-      $svg = $('svg'),
+  var $svg = $('svg'),
       $root = $('.root'),
       frameTime = (1000/30).toFixed(2)*1,
       flowers = {},
-      dots=[],
       shapes=[],
-      $dot,
       timer=-1,
       templates={},
       aniNodeList = new NodeList(),
-      aniNum=0,
       flowerNum = 20,
-      trackPoint = 'trackPoint',
+      trialNum = 10;
       colors=["#f44336","#e91e63","#9c27b0","#673ab7","#3f51b5","#00bcd4","#03a9f4","#009688","#4caf50","#8bc34a","#cddc39","#ffeb3b","#ffc107","#ff9800","#ff5722","#795548","#9e9e9e","#607d8b"];
-var id = 10;
-  init();
 
+  init();
   function init(){
     loadTemplate();
     $svg.on('click',onClickSVG);
@@ -40,16 +35,32 @@ var id = 10;
       timer = -1;
     }
   }
+
   function actAni(){
-    var i,item,finish,prev;
-    console.log("aniNodeList.count="+aniNodeList.count);
-    console.log("shape"+$('.root .template').length);
+    var i,item,finish,prev,x,y,j,finish2;
+    // console.log("aniNodeList.count="+aniNodeList.count);
+    // console.log("shape"+$('.root .template').length);
     item = aniNodeList.head;
     while(true){
       if(item==null)
         break;
-      finish = item.data.moveInfo.update(frameTime);
-      setPos(item.data);
+      finish = true;
+      if(item.data.moveInfo){
+        finish = item.data.moveInfo.update(frameTime);
+        x = item.data.moveInfo.x;
+        y = item.data.moveInfo.y;
+        setTransPos(item.data,x,y);
+        // console.log("t="+item.data.moveInfo.time);
+      }
+      if(item.data.propertyAnis){
+        for(var i in item.data.propertyAnis){
+          finish2 = item.data.propertyAnis[i].update(frameTime);
+          finish = finish && finish2;
+          setProperty(item.data,i,item.data.propertyAnis[i].cur[0]);
+          // console.log("t="+item.data.propertyAnis[i].time);
+          // console.log(i+"="+item.data.propertyAnis[i].cur[0]);
+        }
+      }
       prev = item;
       item = item.next;
       if(finish){
@@ -63,13 +74,28 @@ var id = 10;
     }
   }
   function createFireworks(svgPos){
-    var node,i;
+    var node,i,j,trail,color,opacity,delay;
     for(i=0;i<flowerNum;++i){
-      node = getRandomFlower();
-      setColor(node.data);
-      setFlyParams(node.data,svgPos);
+      node = getRandomFlower("circle");
+      setMoveAniInfo(node.data,svgPos);
+      color = getRandomColor();
+      setProperty(node.data,'fill',color);
+      setProperty(node.data,'fill-opacity',1);
+      setPropertyAni(node.data,'fill-opacity',[1],[0],3000,0);
+      for(j=1;j<=trialNum;++j){
+        trail = getRandomFlower(node.data.shape);
+        trail.data.moveInfo = node.data.moveInfo.clone();
+        trail.data.moveInfo.delay = frameTime*j;
+        trail.data.moveInfo.time = 0;
+        setProperty(trail.data,'fill',color);
+        opacity = (1-j/(trialNum));
+        setProperty(trail.data,
+          'fill-opacity',opacity);
+        setPropertyAni(trail.data,'fill-opacity',[opacity],[0],3000,0);
+      }
     }
   }
+
   function loadTemplate(){
     var i,$item,shape,$temps;
     $temps = $('.template');
@@ -85,29 +111,40 @@ var id = 10;
     }
   }
 
-  function setPos($flower){
-    var x,y,temp;
-    x = $flower.moveInfo.x;
-    y = $flower.moveInfo.y;
-    $flower.attr("transform","translate("+x+","+y+")");
+
+  function setTransPos($ele,x,y){
+    $ele.attr("transform","translate("+x+","+y+")");
   }
 
-  function setColor($flower){
-    $flower.attr("fill",getRandomColor());
+  function setProperty($ele,propertyName,value){
+    $ele.attr(propertyName,value);
+    $ele[propertyName] = value;
   }
 
-  function setFlyParams($flower,svgPos){
+  function setMoveAniInfo($flower,svgPos){
     var x0 = svgPos.x,
         y0 = svgPos.y,
         vx0 = getRandom(-0.2,0.2),//0.1
         vy0 = getRandom(-0.2,0.2),//0.53332 6s
         g = 0.00015,
         delay = 0,
-        dur = 6000;
+        dur = 4000;
     if($flower.moveInfo){
       $flower.moveInfo.reset(x0,y0,vx0,vy0,g,dur,delay);
     }else{
       $flower.moveInfo = new MoveInfo(x0,y0,vx0,vy0,g,dur,delay);
+    }
+  }
+  function setPropertyAni($node,name,begin,end,dur,delay){
+    if(!$node || !name)
+      return;
+    if(!$node.propertyAnis){
+      $node.propertyAnis = {};
+    }
+    if(name in $node.propertyAnis){
+      $node.propertyAnis[name].reset(begin,end,dur,delay);
+    }else{
+      $node.propertyAnis[name] = new PropertyAniInfo(name,begin,end,dur,delay);
     }
   }
   function PropertyAniInfo(name,begin,end,dur,delay){
@@ -118,8 +155,8 @@ var id = 10;
     this.dur;
     this.delay;
     this.time;
-    this.reset(begin,end,dur,delay);
-    this.reset = function(){
+
+    this.reset = function(begin,end,dur,delay){
       this.begin = begin;
       this.cur = begin;
       this.end = end;
@@ -131,9 +168,8 @@ var id = 10;
       var k,i
           finish =false;
       if(this.time < this.delay + this.dur){
-        if(this.time > this.delay){
-          if(Math.abs(this.time-this.dur)<frameTime){
-            this.cur = this.end;
+        if(this.time >= this.delay){
+          if(Math.abs(this.time-this.dur - this.delay)<frameTime){
             for(i=0;i<this.cur.length;++i){
               this.cur[i] = this.end[i];
             }
@@ -142,6 +178,7 @@ var id = 10;
             for(i=0;i<this.cur.length;++i){
               this.cur[i] = this.begin[i] +
                 (this.end[i]-this.begin[i])*k;
+              this.cur[i] = this.cur[i].toFixed(2)*1;
             }
           }
         }
@@ -151,7 +188,7 @@ var id = 10;
       }
       return finish;
     };
-
+    this.reset(begin,end,dur,delay);
   }
   function MoveInfo(x0,y0,vx0,vy0,g,dur,delay){
     this.x0 = x0;
@@ -165,6 +202,7 @@ var id = 10;
     this.g = g;
     this.dur = dur;
     this.delay = delay;
+
     this.update = function(delta){
       var finish = false;
       if(this.time < this.delay + this.dur){
@@ -176,6 +214,8 @@ var id = 10;
             this.vy0 + this.g*(this.time-this.delay);
           this.y = this.y0 +
             (this.vy0+this.vy)/2*(this.time - this.delay);
+          this.x = this.x.toFixed(1);
+          this.y = this.y.toFixed(1);
           //console.log('x,y='+this.x+" "+this.y);
         }
         this.time += delta;
@@ -196,6 +236,10 @@ var id = 10;
       this.g = g;
       this.dur = dur;
       this.delay = delay;
+    };
+    this.clone = function(){
+      return new MoveInfo(this.x0,this.y0,
+        this.vx0,this.vy0,this.g,this.dur,this.delay);
     };
   }
 
