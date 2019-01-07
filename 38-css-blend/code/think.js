460ae719873d004126c57cc59c8cfd2481cc7055
kong66 <konglingbin66@outlook.com>
2019-01-07 19:06:24 +0800

    38

--- 38-css-blend/code/think.js
+++ 38-css-blend/code/think.js
@@ -1,6 +1,39 @@
 jQuery(function($){
+  var x,y,
+      drag=false,
+      $circles = $('.trails'),
+      $temp = $('.circle.template');
 
-  
+  $(document).on('mousedown',mousedown);
+  $(document).on('mouseup',mouseup);
+  $(document).on('mousemove',mousemove);
+
+  function mousedown(e){
+    drag = true;
+  }
+
+  function mousemove(e){
+    if(drag){
+      x = e.clientX;
+      y = e.clientY;
+      var $new = $temp.clone();
+      $circles.append($new);
+      $new.removeClass('template');
+      $new.css({
+        left:x+"px",
+        top:y+"px"
+      });
+      $new.on('animationend',onAniFinished);
+    }
+  }
+
+  function mouseup(){
+    drag = false;
+  }
+
+  function onAniFinished(){
+    $(this).remove();
+  }
 
 
 });
