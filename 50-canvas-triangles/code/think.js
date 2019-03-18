061b2ab756aa03f885390ad4c7236c2f9d607adc
konglingbin <konglingbin66@outlook.com>
2019-03-18 15:15:55 +0800

    50

--- 50-canvas-triangles/code/think.js
+++ 50-canvas-triangles/code/think.js
@@ -1,66 +1,3 @@
-// window.requestAnimFrame = (function() {
-//   return (
-//     window.requestAnimationFrame ||
-//     window.webkitRequestAnimationFrame ||
-//     window.mozRequestAnimationFrame ||
-//     window.oRequestAnimationFrame ||
-//     window.msRequestAnimationFrame ||
-//     function(/* function */ callback, /* DOMElement */ element) {
-//       window.setTimeout(callback, 1000 / 60);
-//     }
-//   );
-// })();
-//
-// window.requestTimeout = function(fn, delay) {
-//   if (
-//     !window.requestAnimationFrame &&
-//     !window.webkitRequestAnimationFrame &&
-//     !(
-//       window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame
-//     ) && // Firefox 5 ships without cancel support
-//     !window.oRequestAnimationFrame &&
-//     !window.msRequestAnimationFrame
-//   )
-//     return window.setTimeout(fn, delay);
-//
-//   var start = new Date().getTime(),
-//     handle = new Object();
-//
-//   function loop() {
-//     var current = new Date().getTime(),
-//       delta = current - start;
-//
-//     delta >= delay ? fn.call() : (handle.value = requestAnimFrame(loop));
-//   }
-//
-//   handle.value = requestAnimFrame(loop);
-//   return handle;
-// };
-//
-// window.requestInterval = function(fn, delay) {
-//   var requestAnimFrame = (function() {
-//       return (
-//         window.requestAnimationFrame ||
-//         function(callback, element) {
-//           window.setTimeout(callback, 1000 / 60);
-//         }
-//       );
-//     })(),
-//     start = new Date().getTime(),
-//     handle = {};
-//   function loop() {
-//     handle.value = requestAnimFrame(loop);
-//     var current = new Date().getTime(),
-//       delta = current - start;
-//     if (delta >= delay) {
-//       fn.call();
-//       start = new Date().getTime();
-//     }
-//   }
-//   handle.value = requestAnimFrame(loop);
-//   return handle;
-// };
-
 
 var canvas,ctx,h,w,size,
     randomRatio = 2,
@@ -80,7 +17,7 @@ var canvas,ctx,h,w,size,
     createTriangles();
 
     update();
-    //requestInterval(update, 30);
+    
   }
   function resizeFunc() {
     resize();
