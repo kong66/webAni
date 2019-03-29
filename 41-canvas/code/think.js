7e43958c61d6ed0d4a234b41a262293f2eb94fc5
konglingbin66@outlook.com <konglingbin66@outlook.com>
2019-03-29 11:57:16 +0800

    45

--- 41-canvas/code/think.js
+++ 41-canvas/code/think.js
@@ -1,49 +1,261 @@
-jQuery(function($){
-  var context = document.getElementById('canvas').getContext('2d'),
-    circles = [];
+window.requestAnimFrame = (function() {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function(/* function */ callback, /* DOMElement */ element) {
+      window.setTimeout(callback, 1000 / 60);
+    }
+  );
+})();
 
-  init();
-  function init(){
-    createCirclesInfo();
-    setInterval(update, 1000 / 60);
+window.requestTimeout = function(fn, delay) {
+  if (
+    !window.requestAnimationFrame &&
+    !window.webkitRequestAnimationFrame &&
+    !(
+      window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame
+    ) && // Firefox 5 ships without cancel support
+    !window.oRequestAnimationFrame &&
+    !window.msRequestAnimationFrame
+  )
+    return window.setTimeout(fn, delay);
+
+  var start = new Date().getTime(),
+    handle = new Object();
+
+  function loop() {
+    var current = new Date().getTime(),
+      delta = current - start;
+
+    delta >= delay ? fn.call() : (handle.value = requestAnimFrame(loop));
   }
 
-  function createCirclesInfo(){
-    for (var i=0; i < 100; ++i) {
-       circles[i] = {
-           x: context.canvas.width * Math.random(),
-           y: context.canvas.height * Math.random(),
-           vx: 4*Math.random()-2,
-           vy: 4*Math.random()-2,
-           r: 50*Math.random()+10,
-           color: 'rgba(' +
-                (Math.random()*255).toFixed(0) + ', ' +
-                (Math.random()*255).toFixed(0) + ', ' +
-                (Math.random()*255).toFixed(0) + ', '
-                +(0.5+0.5*Math.random()).toFixed(1)+')'};
+  handle.value = requestAnimFrame(loop);
+  return handle;
+};
+
+window.requestInterval = function(fn, delay) {
+  var requestAnimFrame = (function() {
+      return (
+        window.requestAnimationFrame ||
+        function(callback, element) {
+          window.setTimeout(callback, 1000 / 60);
+        }
+      );
+    })(),
+    start = new Date().getTime(),
+    handle = {};
+  function loop() {
+    handle.value = requestAnimFrame(loop);
+    var current = new Date().getTime(),
+      delta = current - start;
+    if (delta >= delay) {
+      fn.call();
+      start = new Date().getTime();
+    }
+  }
+  handle.value = requestAnimFrame(loop);
+  return handle;
+};
+
+
+var canvas,ctx,h,w,size,
+    randomRatio = 0.9,
+    dodgeRatio = 0.3,
+    maxBrightness = 80,
+    mouseFastX = 0,mouseFastY = 0,
+    mouseX = 0,mouseY = 0,
+    triangles = [],
+    dynamicGrid = [],
+    randomValues = [],
+    grid = [],
+    columns,rows;
+
+
+
+  function init() {
+    canvas = document.createElement("canvas");
+    document.getElementById("bg").appendChild(canvas);
+    ctx = canvas.getContext("2d");
+    window.onresize = resizeFunc;
+    canvas.onmousemove = onmouseMove;
+    resize();
+    createGrid();
+    createTriangles(grid);
+    setRandomTriangleValue();
+    drawTriangles();
+    drawText();
+    requestInterval(update, 30);
+  }
+  function resizeFunc() {
+    resize();
+    createGrid();
+    createTriangles(grid);
+    setRandomTriangleValue();
+  }
+
+  init();
+  function resize() {
+    h = canvas.height = window.innerHeight;
+    w = canvas.width = window.innerWidth;
+    size = w >= h ? w / 16 : h / 16;
+    dodgeThreshold= size*3;
+    columns = Math.round(1.2 * w / size);
+    rows = Math.round(1.2 * h / size);
+  }
+
+  function createGrid() {
+    var row;
+    grid = [];
+      for (j = -1- rows / 2; j < rows / 2 + 1; j++) {
+        row = [];
+        for (i = -1- columns / 2; i < columns / 2 + 1; i++) {
+          x = i * size  + Math.random() * size* randomRatio;
+          y = j * size + Math.random() * size* randomRatio;
+          row.push([x, y]);
+      }
+      grid.push(row);
     }
   }
 
-  function update(){
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    circles.forEach(function(circle) {
-      context.beginPath();
-      context.arc(circle.x, circle.y, circle.r, 0, Math.PI*2, false);
-      context.fillStyle = circle.color;
-      context.fill();
-      move(circle);
-    });
-  }
-  function move(circle) {
-     if (circle.x + circle.vx + circle.r > context.canvas.width ||
-         circle.x + circle.vx - circle.r < 0)
-        circle.vx = -circle.vx;
+  function createTriangles(grid) {
+    var row,point1,point2,point3,point4;
+    triangles = [];
+    for (row = 0; row < grid.length - 1; row ++) {
+      for (j = 0; j < grid[row].length - 1; j ++) {
 
-     if (circle.y + circle.vy + circle.r > context.canvas.height ||
-         circle.y + circle.vy - circle.r  < 0)
-        circle.vy= -circle.vy;
+        point1 = grid[row][j];
+        point2 = grid[row + 1][j];
+        point3 = grid[row+1][j + 1];
+        point4 = grid[row][j+1];
 
-     circle.x += circle.vx;
-     circle.y += circle.vy;
+        triangles.push([point1,point2,point4]);
+        triangles.push([point2,point3,point4]);
+
+      }
+    }
   }
-});
+
+  function createDynamicGrid() {
+    var d,dx,dy,
+        dodgeLength,
+        offsetX,
+        offsetY;
+    dynamicGrid = cloneArray(grid);
+    for (i = 0; i < dynamicGrid.length - 1; i++) {
+      for (j = 0; j < dynamicGrid[i].length - 1; j++) {
+        dx = dynamicGrid[i][j][0] - mouseX;
+        dy = dynamicGrid[i][j][1] - mouseY;
+        dy == 0 ? (dy = 0.001) : dy;
+        dx == 0 ? (dx = 0.001) : dx;
+        d = Math.sqrt(dx * dx + dy * dy);
+
+        if (d < dodgeThreshold) {
+          dodgeLength = (dodgeThreshold -  d) * dodgeRatio;
+          offsetX = dodgeLength * dx/d;
+          offsetY = dodgeLength * dy/d;
+          dynamicGrid[i][j][0] += offsetX;
+          dynamicGrid[i][j][1] += offsetY;
+        }
+      }
+    }
+  }
+
+
+
+  function cloneArray(arr) {
+    var len = arr.length;
+    var newArr = new Array(len);
+    for (var i = 0; i < len; i++) {
+      if (Array.isArray(arr[i])) {
+        newArr[i] = cloneArray(arr[i]);
+      } else {
+        newArr[i] = arr[i];
+      }
+    }
+    return newArr;
+  }
+
+  function setRandomTriangleValue() {
+    randomValues = [];
+    for (i = 0; i < triangles.length; i++) {
+      randomValues[i] = Math.random();
+    }
+  }
+
+  function caculate_brightness(points) {
+    var brightness,i,avarage=0;
+    for (i = 0; i < points.length; i++) {
+      avarage += Math.abs(points[i][0]) + Math.abs(points[i][1]) ;
+    }
+    avarage /=3;
+    brightness = (w+h - avarage)/(w+h)*maxBrightness ;
+    return brightness;
+  }
+
+
+  function drawTriangle(p, rand) {
+    var hue,sumDistance,color,brightness;
+    ctx.save();
+    ctx.beginPath();
+    ctx.translate(w/2,h/2);
+    ctx.moveTo(p[0][0], p[0][1]);
+    ctx.lineTo(p[1][0], p[1][1]);
+    ctx.lineTo(p[2][0], p[2][1]);
+    ctx.closePath();
+
+    hue = rand * 10 + 220;
+    brightness = caculate_brightness(p);
+    color = "hsl(" + hue + ",50%, " + brightness + "%)";
+
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+    ctx.stroke();
+    ctx.fill();
+    ctx.restore();
+  }
+
+  function drawTriangles() {
+    var i;
+    for (i=0;i < triangles.length;++i) {
+      drawTriangle(triangles[i], randomValues[i]);
+    }
+  }
+
+  function drawText(){
+    ctx.save();
+    ctx.fillStyle= 'rgba(255,255,255,.3)';
+    ctx.textAlign= 'center';
+    ctx.textBaseline = 'middle';
+    ctx.font= '200px impact';
+    ctx.fillText('KONG66', canvas.width/2, canvas.height/2);
+    ctx.restore();
+  }
+
+  function update() {
+    createDynamicGrid(grid);
+    createTriangles(dynamicGrid);
+    var dMouseX = mouseFastX - mouseX;
+    var dMouseY = mouseFastY - mouseY;
+    mouseX += dMouseX/10;
+    mouseY += dMouseY/10;
+    drawTriangles();
+    drawText();
+  }
+
+  function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+  }
+
+  function onmouseMove(evt) {
+    var mousePos = getMousePos(canvas, evt);
+    mouseFastX = mousePos.x - w / 2;
+    mouseFastY = mousePos.y - h / 2;
+  }
